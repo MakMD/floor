3dc97b54701da265f5b73d093d8852c5208eb19c
M <168857369+Note-m@users.noreply.github.com>
2024-10-03 12:23:45 -0700

    Update TableDetailsPage.jsx

--- src/Pages/TableDetailsPage.jsx
+++ src/Pages/TableDetailsPage.jsx
@@ -224,7 +224,7 @@ const TableDetailsPage = () => {
           <h1>FLOORING BOSS LTD.</h1>
           <p>422 ALLARD BLVD SW, EDMONTON, ALBERTA, T6W3S7</p>
           <p>
-            Contact Mykhailo: (587) 937 7862 | Contact Miroslav: (825) 461 1950
+            Contact Mykhailo: (587) 937 7862 | Contact Myroslav: (825) 461 1950
           </p>
           <p>Email: Flooringm8pservice@gmail.com</p>
           <p>GST: 704201813 RT 0001 | WCB: 9839473</p>
