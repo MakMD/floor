b412a1960007c4f1f733424e3457b29f6621cc61
M <168857369+Note-m@users.noreply.github.com>
2024-10-03 13:10:47 -0700

    +

--- src/Pages/CompanyTablesPage.jsx
+++ src/Pages/CompanyTablesPage.jsx
@@ -178,10 +178,14 @@ const CompanyTablesPage = () => {
   return (
     <div className={styles.pageContainer}>
       {/* Кнопка "Назад" */}
-      <button className={styles.backButton} onClick={() => navigate(-1)}>
-        Back
-      </button>
-
+      <div className={styles.backEditBtnCont}>
+        <button className={styles.backButton} onClick={() => navigate(-1)}>
+          Back
+        </button>
+        <button onClick={toggleEditMode} className={styles.editButton}>
+          {isEditing ? "Stop Editing" : "Edit"}
+        </button>
+      </div>
       <h2 className={styles.pageTitle}>{companyName} - Tables</h2>
       {error && <p className={styles.errorMessage}>{error}</p>}
 
